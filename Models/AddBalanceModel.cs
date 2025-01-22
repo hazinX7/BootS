672d8757554559df6659b71dfb8ad3c46136778c
hazinX7 <adm.owner@mail.ru>
2025-01-22 19:01:41 +0700

    Pay System

--- Models/AddBalanceModel.cs
+++ Models/AddBalanceModel.cs
@@ -4,6 +4,9 @@ namespace BootS.Models
 {
     public class AddBalanceModel
     {
+        public string CardNumber { get; set; }
+        public string ExpiryDate { get; set; }
+        public string CVV { get; set; }
         [Required]
         [Range(1, 750000)]
         public decimal Amount { get; set; }
